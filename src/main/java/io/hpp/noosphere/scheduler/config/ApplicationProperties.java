3774ebf7e18e15013c5a967210547e2579d8225b
jjang <yoonjoo.jang@gmail.com>
2025-11-11 21:59:45 +0900

    #2 Integrate Web3j and scheduled commitment generation tasks
    - Introduced `Web3jConfiguration` to configure Web3j bean.
    - Added properties to support Ethereum node configuration.
    - Implemented `CommitmentGenerationService` with a scheduled task based on configured cron expression.
    - Updated dependencies to include Web3j libraries.

--- src/main/java/io/hpp/noosphere/scheduler/config/ApplicationProperties.java
+++ src/main/java/io/hpp/noosphere/scheduler/config/ApplicationProperties.java
@@ -3,7 +3,7 @@ package io.hpp.noosphere.scheduler.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * Properties specific to Noosphere Scheduler.
+ * Properties specific to noosphereScheduler.
  * <p>
  * Properties are configured in the {@code application.yml} file.
  * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
@@ -33,5 +33,50 @@ public class ApplicationProperties {
             this.asyncStart = asyncStart;
         }
     }
-    // jhipster-needle-application-properties-property-class
+    private final ScheduleTask scheduleTask = new ScheduleTask();
+    private final Ethereum ethereum = new Ethereum();
+
+
+    public ScheduleTask getScheduleTask() {
+        return scheduleTask;
+    }
+
+    public Ethereum getEthereum() {
+        return ethereum;
+    }
+
+    public static class ScheduleTask {
+
+        private final CommitmentGeneration commitmentGeneration = new CommitmentGeneration();
+
+        public CommitmentGeneration getCommitmentGeneration() {
+            return commitmentGeneration;
+        }
+
+        public static class CommitmentGeneration {
+
+            private String cron;
+
+            public String getCron() {
+                return cron;
+            }
+
+            public void setCron(String cron) {
+                this.cron = cron;
+            }
+        }
+    }
+
+    public static class Ethereum {
+
+        private String nodeUrl;
+
+        public String getNodeUrl() {
+            return nodeUrl;
+        }
+
+        public void setNodeUrl(String nodeUrl) {
+            this.nodeUrl = nodeUrl;
+        }
+    }
 }
